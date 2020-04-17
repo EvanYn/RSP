a0cf4317426bd66b1bb35d861ae7b973f443fa86
EvanYn <yuangesheng@gmail.com>
2020-04-17 11:00:41 +0800

    modify binomial computation method

--- frsp/DBF/binomialTaper.m
+++ frsp/DBF/binomialTaper.m
@@ -1,27 +1,32 @@
 clear; clc; close all;
 
 %%
-N = [8 16 32];
-for n = 1:length(N)
+N = [16 32 64];
+for i = 1:length(N)
     
     dl = 1/2;
     
-    w = ones(N(n),1);
+    n = N(i) - 1;
+    w = ones(n+1,1);
+    w(1) = 1;
     
-    for i = 1:N(n)
-        w(i) = nchoosek(N(n)-1,i-1);
+    for k = 1:n
+        w(k+1) = w(k)*(n-(k-1))/k;
     end
     
     %%
     theta = -90:0.01:90;
     fsp = dl*sind(theta);
-    A = exp(1j*2*pi*fsp.*(0:N(n)-1)');
+    A = exp(1j*2*pi*fsp.*(0:N(i)-1)');
     
     P = w'*A;
     P = abs(P)/max(abs(P));
     
-    plot(fsp,P, 'DisplayName', ['N =', num2str(N(n))])
+    plot(fsp,P, 'DisplayName', ['N = ', num2str(N(i))])
     hold on
 end
 legend
 grid on
+xlabel('spatial frequency (Cycle/Sample)')
+ylabel('Amplitude')
+title('beampattern of binomial taper')
